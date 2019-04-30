26cadb11feb0e4a8de24a539fe834cda6a7d211e
Thomas MONTERO <thomas99.montero@gmail.com>
2019-04-30 14:32:44 +0200

    added the auto-win against teh fictive "Bye" player

--- tournament/Match.py
+++ tournament/Match.py
@@ -7,23 +7,30 @@ class Match:
 		self.results = {"p1": 0, "pat": 0, "p2": 0}
 
 	def play(self):
-		while self.results["p1"] < self.bo//2+1 and self.results["pat"] < self.bo//2+1 and self.results["p2"] < self.bo//2+1 and (self.results["p1"]+self.results["pat"]+self.results["p2"]) < self.bo:
-			self.Game.play()
+		if self.p1.name == "bye":
+			self.p2.victory()
 
-			if self.Game.results == 1:
-				self.results["p1"] += 1
-			elif self.Game.results == 2:
-				self.results["p2"] += 1
-			else:
-				self.results["pat"] += 1
+		elif self.p2.name == "bye":
+			self.p1.victory()
 
-		self.p1.hasPlayed.append(self.p2)
-		self.p2.hasPlayed.append(self.p1)
-
-		if self.results["p1"] > self.results["p2"] and self.results["p1"] > self.results["pat"]:
-			self.p1.victoire()
-		elif self.results["p2"] > self.results["p1"] and self.results["p2"] > self.results["pat"]:
-			self.p2.victoire()
 		else:
-			self.p1.egalite()
-			self.p2.egalite()
+			while self.results["p1"] < self.bo//2+1 and self.results["pat"] < self.bo//2+1 and self.results["p2"] < self.bo//2+1 and (self.results["p1"]+self.results["pat"]+self.results["p2"]) < self.bo:
+				self.Game.play()
+
+				if self.Game.results == 1:
+					self.results["p1"] += 1
+				elif self.Game.results == 2:
+					self.results["p2"] += 1
+				else:
+					self.results["pat"] += 1
+
+			self.p1.hasPlayed.append(self.p2)
+			self.p2.hasPlayed.append(self.p1)
+
+			if self.results["p1"] > self.results["p2"] and self.results["p1"] > self.results["pat"]:
+				self.p1.victory()
+			elif self.results["p2"] > self.results["p1"] and self.results["p2"] > self.results["pat"]:
+				self.p2.victory()
+			else:
+				self.p1.draw()
+				self.p2.draw()
